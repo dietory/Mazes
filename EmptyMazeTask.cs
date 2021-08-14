a5adf0898132c5cdb2db31f5b9b06fed9a3a2861
Robert I <robertorbbit@gmail.com>
2021-08-14 12:15:07 +0500

    Добавить решение задачи

--- EmptyMazeTask.cs
+++ EmptyMazeTask.cs
@@ -4,6 +4,23 @@
 	{
 		public static void MoveOut(Robot robot, int width, int height)
 		{
+			int stepsW = 1;
+			int stepsH = 1;
+			while(stepsH <height-2 || stepsW < width -2)
+            {
+				if (stepsW < width - 2)
+                {
+					robot.MoveTo(Direction.Right);
+					stepsW++;
+				}
+					
+				if (stepsH < height - 2)
+                {
+					robot.MoveTo(Direction.Down);
+					stepsH++;
+				}
+					
+            }
 		}
 	}
 }
