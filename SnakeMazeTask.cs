eaf7a8e1d9a70568dc393a119ef35fa678e680cd
Robert I <robertorbbit@gmail.com>
2021-08-14 13:03:54 +0500

    Добавить решение для 2 задачи

--- SnakeMazeTask.cs
+++ SnakeMazeTask.cs
@@ -4,6 +4,25 @@
 	{
 		public static void MoveOut(Robot robot, int width, int height)
 		{
+			int stepsH = 1;
+			Direction direction = Direction.Right;
+			while(stepsH <= height - 2)
+            {
+				MoveToNextSide(robot, direction, width - 3);
+				if (stepsH >= height - 2) break;
+				robot.MoveTo(Direction.Down);
+				robot.MoveTo(Direction.Down);
+				stepsH += 2;
+				direction = direction == Direction.Right ? Direction.Left : Direction.Right;
+			}
 		}
+
+		public static void MoveToNextSide(Robot robot, Direction direction, int steps)
+        {
+			for(int i = 1; i <= steps; i++)
+            {
+				robot.MoveTo(direction);
+            }
+        }
 	}
 }
